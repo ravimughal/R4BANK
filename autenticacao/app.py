bece5b339b38fb500024a9f7e1e256c582e602de
Ravi Mughal <ravimughal@hotmail.com>
2023-09-29 18:37:11 -0300

    vendo saldo na conta

--- autenticacao/app.py
+++ autenticacao/app.py
@@ -1,15 +1,29 @@
-import classes
+import database
 
 def principal_app(cliente_objeto):
+    conn = database.connect_db()
+    cpf = cliente_objeto.cpf
 
-    cliente_objeto.show()
-
-    app_comands = str(input("""
+    app_comands = int(input("""
 Selecione uma opção
 1 - Ver Saldo
 2 - Deposito 
 3 - Sacar
+4 - Sair
 """))
+    
+    if app_comands == 1:
+        saldo = database.saldo(conn, cpf)
+        print(f"O saldo da conta é: {saldo}")
+
+    elif app_comands == 2:
+        depositar()
+
+
+
+
+def depositar():
+    print("depositei")
 
 
 def inserir_nome_cpf(nome, cpf):
